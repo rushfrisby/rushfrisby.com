cca701dd29cac4131d2e527de91378f8fc04b560
Rush Frisby <rush.frisby@derivesystems.com>
2018-11-25 19:27:05 -0500

    publish

---
+++ publish.sh
@@ -0,0 +1,5 @@
+dotnet /c/dev/local/RushBlog/RushBlog.StaticSiteGenerator/bin/Debug/netcoreapp2.1/RushBlog.StaticSiteGenerator.dll /c/dev/github/rushfrisby.com/
+cd /c/dev/github/rushfrisby.com/
+git add -A
+git commit -m "publish"
+git push

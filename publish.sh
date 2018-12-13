3eb03793040810742843398023df65e4252c7d8b
Rush Frisby <rush.frisby@derivesystems.com>
2018-12-13 13:50:28 -0500

    publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-dotnet /c/dev/local/RushBlog/RushBlog.StaticSiteGenerator/bin/Debug/netcoreapp2.1/RushBlog.StaticSiteGenerator.dll /c/dev/github/rushfrisby.com/
+dotnet /c/dev/github/RushBlog/RushBlog.StaticSiteGenerator/bin/Debug/netcoreapp2.1/RushBlog.StaticSiteGenerator.dll /c/dev/github/rushfrisby.com/
 cd /c/dev/github/rushfrisby.com/
 SqlCmd -E -S "(localdb)\mssqllocaldb" -Q "BACKUP DATABASE [RushBlog] TO DISK='c:\dev\github\rushfrisby.com\_db.bak'"
 rm _db.zip
